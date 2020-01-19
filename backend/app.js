1a0bc72a75e4e2eefc7ac178c7312396639b09ec
Viktor Rusev <vikrusev257@gmail.com>
2020-01-20 00:54:56 +0200

    backend seperated and build config fixes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function() {
-  console.log('Server listening on port: 3000');
+app.all('*', (req, res) => {
+  console.log(process.env);
+  res.sendFile(path.join(__dirname, 'public', 'angular-root.html'));
+})
+
+app.listen(process.env.PORT, function() {
+  console.log(`Server listening on port: ${process.env.PORT}`);
 });
