a4e3e16b402bc1215a6ac16287ea1e68998b6ea3
Viktor Rusev <vikrusev257@gmail.com>
2020-02-05 12:23:27 +0200

    renamed common models

--- common/@types/UserModel.d.ts
+++ common/@types/UserModel.d.ts
@@ -1,41 +1,46 @@
-interface BasicUserModel {
-    email: String,
+interface IBasicUser {
+    username: String,
     password: String,
     lastLogin: Date, // Date of last successful login
-    profile: UserProfile
+    profile: IUserProfile
 }
 
-interface UserWithHistories extends BasicUserModel {
-    passwordHistory: PasswordHistory[],
-    loginHistory: LoginHistory[],
-    raceHistory: RaceHistory[]
+interface IUserWithHistories extends IBasicUser {
+    passwordHistory: IPasswordHistory[],
+    loginHistory: ILoginHistory[],
+    raceHistory: IRaceHistory[]
 }
 
-interface UserProfile {
-    /* Visible username of the player */
-    username: String,
+interface IUserDocumentModel extends IUserWithHistories, Document {
+    fullname(): string
+}
+
+interface IUserProfile {
+    /* Personal data of the player */
+    email: String,
     firstName?: String,
     lastName?: String,
-    birthDate?: Date
+    birthDate?: Date,
+    visible?: Boolean // false by default
 }
 
-interface PasswordHistory {
+interface IPasswordHistory {
     password: String,
     changeDate: Date
 }
 
-interface LoginHistory {
+interface ILoginHistory {
     date: Date,
     invalidLogins: Number // Used to block login attempts if needed
 }
 
-interface Oponent {
+interface IOponent {
     userId: String,
     username: String
 }
 
-interface RaceHistory {
-    oponent: Oponent,
+interface IRaceHistory {
+    oponent: IOponent,
     date: Date
     status: Boolean, // Wheather the battle is a win or loss
     type: String // Type of race - attack, defence, bot attack
