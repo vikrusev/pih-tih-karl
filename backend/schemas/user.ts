4394135a874cd5144e6c3cf5c5317732dd8249c7
Viktor Rusev <vikrusev257@gmail.com>
2020-02-09 22:22:32 +0200

    userSchema post find/findOne to remove password

--- backend/schemas/user.ts
+++ backend/schemas/user.ts
@@ -28,6 +28,20 @@ userSchema.pre<IUserDocumentModel>('save', async function (next): Promise<void> 
     next();
 });
 
+const processFindQuery = (users) => {
+    if (users) {
+        if (!Array.isArray(users)) {
+            users = [ users ];
+        }
+        users.forEach((user) => {
+            delete user.password;
+        });
+    }
+}
+
+userSchema.post<IUserDocumentModel>('find', processFindQuery);
+userSchema.post<IUserDocumentModel>('findOne', processFindQuery);
+
 userSchema.methods.isValidPassword = async function (password: string): Promise<boolean> {
     const compare = await bcrypt.compare(password, this.password);
     return compare;
