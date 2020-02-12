1e2ee5b3b11b88449abb58d7f6dd6a3de60b74ce
Viktor Rusev <vikrusev257@gmail.com>
2020-02-12 14:24:18 +0200

    dummy profile birthdate fix

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import moment from 'moment';
 
 import { UsersService } from 'src/app/services/users.service';
 
@@ -12,20 +13,29 @@ export class ProfileComponent implements OnInit {
     user: IExtendedUser = null;
     profileError: String = null;
 
+    birthDate: string = null;
+
     constructor(private userService: UsersService) { }
 
     ngOnInit() {
         this.user = this.userService.getCurrentUser();
+        this.birthDate = this.setUserBirthDate(this.user.profile.birthDate);
     }
 
     async updateData() {
         try {
+            this.user.profile.birthDate = new Date(this.birthDate);
+
             const newUser: IExtendedUser = await this.userService.updateUser(this.user);
             this.userService.setCurrentUser(newUser);
         }
         catch (err) {
             this.profileError = err.error.message;
         }
+    }
+
+    private setUserBirthDate(birthDate: Date): string {
+        return moment(birthDate).format('YYYY-MM-DD');
     }
 
 }
